1bb19d8b4345de0aae9489b00e3399e6664f2f0b
Filipe Serro <filipeserro@Filipes-MacBook-Pro.local>
2021-05-04 09:51:06 +0200

    added data and buynilla.py script

---
+++ buynilla.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+import glob
+
+path = './data/'
+extension = 'csv'
+os.chdir(path)
+result = glob.glob('*.{}'.format(extension))
+
+# this reorders the files chronologically
+order = [1, 3, 4, 2, 0]
+result = [result[i] for i in order]
+print(result)
+
+df = pd.DataFrame()
+for i in result:
+    df_test = pd.read_csv(f'{i}', sep=';', index_col=0, parse_dates=True)
+    df = pd.concat([df, df_test])
